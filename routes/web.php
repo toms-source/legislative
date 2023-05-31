7f7638f2244ec7e1ca992134cb8a0f0e86c83444
eugene2050 <eugenioeugene09@gmail.com>
2023-05-31 11:16:57 +0800

    remove route

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('home');
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
