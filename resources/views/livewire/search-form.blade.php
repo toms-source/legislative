e712cec379d797f3f49b36f0f19116eff80660ce
Gerwin Cristobal <gerwincristobal11@gmail.com>
2023-06-01 22:00:01 +0800

    design

--- resources/views/livewire/search-form.blade.php
+++ resources/views/livewire/search-form.blade.php
@@ -5,20 +5,22 @@
 </div>
 <hr class="hr"/>
 <div class="mt-3">
-        <div class="form-group">
+    <div class="row">
+        <div class="form-group col-sm">
             <label for="date">From:</label>
             <input type="date" class="form-control" id="dateFrom" wire:model="dateFrom">
             @error('date')
                 <span class="text-danger">{{ $message }}</span>
             @enderror
         </div>
-        <div class="form-group ">
+        <div class="form-group col-sm">
             <label for="date">To:</label>
             <input type="date" class="form-control" id="dateTo" wire:model="dateTo">
             @error('date')
                 <span class="text-danger">{{ $message }}</span>
             @enderror
         </div>
+    </div>
         <button type="submit" class="btn btn-success mt-3 col-12" wire:click='searchDates'>Submit</button>
 </div>
 </div>
